3b7f504b4f68802e9fa53df248fc4e8845bd32d4
Illuzion-phuket <56669596+Illuzion-phuket@users.noreply.github.com>
2021-03-09 14:32:20 +0700

    obey permission settings

--- CRM/Casecontribution/Page/CaseTab.php
+++ CRM/Casecontribution/Page/CaseTab.php
@@ -17,14 +17,14 @@ class CRM_Casecontribution_Page_CaseTab {
 	}
 
   public function run() {
-    $case = civicrm_api3('Case', 'getsingle', array('id' => $this->caseId));
-    $caseContributions = civicrm_api3('CaseContribution', 'get', array('case_id' => $this->caseId));
-    $template = CRM_Core_Smarty::singleton();
-    $contributions = array();
-    $qfKey = CRM_Utils_Request::retrieve('key', 'String', CRM_Core_DAO::$_nullObject);
+    if (CRM_Core_Permission::check('access CiviContribute')) {
+      $case = civicrm_api3('Case', 'getsingle', array('id' => $this->caseId));
+      $caseContributions = civicrm_api3('CaseContribution', 'get', array('case_id' => $this->caseId));
+      $template = CRM_Core_Smarty::singleton();
+      $contributions = array();
+      $qfKey = CRM_Utils_Request::retrieve('key', 'String', CRM_Core_DAO::$_nullObject);
+      $permissions = array();
     
-    if (CRM_Core_Permission::check('access contributions')) {
-      $permissions = array(CRM_Core_Permission::VIEW);
       if (CRM_Core_Permission::check('edit contributions')) {
         $permissions[] = CRM_Core_Permission::EDIT;
         $template->assign('allowed_to_add_contribution', true);
@@ -32,47 +32,47 @@ class CRM_Casecontribution_Page_CaseTab {
       if (CRM_Core_Permission::check('delete in CiviContribute')) {
         $permissions[] = CRM_Core_Permission::DELETE;
       }
-    }
-    $mask = CRM_Core_Action::mask($permissions);
+      
+      $mask = CRM_Core_Action::mask($permissions);
 
-    foreach($caseContributions['values'] as $caseContribution) {
-    	try {
-      	$contribution = civicrm_api3('Contribution', 'getsingle', array('id' => $caseContribution['contribution_id']));
-			
-	      $actions = array(
-	        'id' => $contribution['id'],
-	        'cid' => $contribution['contact_id'],
-	        'cxt' => '',
-	      );
-	
-	      $contribution['action'] = CRM_Core_Action::formLink(
-	        CRM_Contribute_Selector_Search::links($this->caseId,
-	          CRM_Core_Action::VIEW,
-	          $qfKey,
-	          'case'
-	        ),
-	        $mask, $actions,
-	        ts('more'),
-	        FALSE,
-	        'contribution.selector.row',
-	        'Contribution',
-	        $contribution['id']
-	      );
-	
-	      $contributions[] = $contribution;
-			} catch (Exception $e) {
-				// Do nothing
-			}
+      foreach($caseContributions['values'] as $caseContribution) {
+        try {
+          $contribution = civicrm_api3('Contribution', 'getsingle', array('id' => $caseContribution['contribution_id']));
+        
+          $actions = array(
+            'id' => $contribution['id'],
+            'cid' => $contribution['contact_id'],
+            'cxt' => '',
+          );
+    
+          $contribution['action'] = CRM_Core_Action::formLink(
+            CRM_Contribute_Selector_Search::links($this->caseId,
+              CRM_Core_Action::VIEW,
+              $qfKey,
+              'case'
+            ),
+            $mask, $actions,
+            ts('more'),
+            FALSE,
+            'contribution.selector.row',
+            'Contribution',
+            $contribution['id']
+          );
+    
+          $contributions[] = $contribution;
+        } catch (Exception $e) {
+          // Do nothing
+        }
 
-    }
-    $template->assign('case_id', $this->caseId);
-    $template->assign('contact_id', reset($case['client_id']));
-    $template->assign('contributionsCount', count($contributions));
-    $template->assign('contributions', $contributions);
+      }
+      $template->assign('case_id', $this->caseId);
+      $template->assign('contact_id', reset($case['client_id']));
+      $template->assign('contributionsCount', count($contributions));
+      $template->assign('contributions', $contributions);
 
-    $tplName = 'CRM/Casecontribution/Page/CaseTab.tpl';
-    CRM_Utils_Hook::alterTemplateFile('CRM_Casecontribution_Page_CaseTab', $this, 'page', $tplName);
-    return $template->fetch($tplName);
+      $tplName = 'CRM/Casecontribution/Page/CaseTab.tpl';
+      CRM_Utils_Hook::alterTemplateFile('CRM_Casecontribution_Page_CaseTab', $this, 'page', $tplName);
+      return $template->fetch($tplName);
+    } 
   }
-
 }
